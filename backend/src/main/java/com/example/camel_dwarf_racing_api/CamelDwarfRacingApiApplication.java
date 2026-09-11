9fad7baa2f7f97f17245ba295ee18711407813ac
ekkogoat <lucas.henao11@gmail.com>
2026-09-11 09:25:14 -0500

    chore: add Spring Boot backend skeleton with core dependencies

---
+++ backend/src/main/java/com/example/camel_dwarf_racing_api/CamelDwarfRacingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.camel_dwarf_racing_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelDwarfRacingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelDwarfRacingApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/camel_dwarf_racing_api/CamelDwarfRacingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.camel_dwarf_racing_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelDwarfRacingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
